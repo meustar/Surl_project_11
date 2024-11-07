016a0d63cdce47e7108e24110919fb50090065d3
meustar <meustar829@gmail.com>
2024-11-07 16:00:55 +0900

    JPA가 실행하는 SQL 로깅

--- src/main/java/com/koreait/surl_project_11/Article.java
+++ src/main/java/com/koreait/surl_project_11/Article.java
@@ -1,17 +1,19 @@
 package com.koreait.surl_project_11;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
-@Entity
+@Entity // 이 클래스로 테이블 만들것이다.
 public class Article {
 
-    @Id // 이거는 primary Key 입니다.
+    @Id // 이 필드를 primary Key로 만들것이다.
     @GeneratedValue(strategy = IDENTITY)    // Auto_increment
     private long id;
     private String title;
+    @Column(columnDefinition = "TEXT", nullable = false)
     private String body;
 }

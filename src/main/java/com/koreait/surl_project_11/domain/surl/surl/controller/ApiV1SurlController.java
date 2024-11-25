31645f7d1bcc2d53ad7c51f991ace6cb1767d32e
meustar <meustar829@gmail.com>
2024-11-25 13:22:33 +0900

    REST API, SURL 다건조회

--- src/main/java/com/koreait/surl_project_11/domain/surl/surl/controller/ApiV1SurlController.java
+++ src/main/java/com/koreait/surl_project_11/domain/surl/surl/controller/ApiV1SurlController.java
@@ -17,6 +17,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/surls")
 @RequiredArgsConstructor
@@ -79,6 +81,27 @@ public class ApiV1SurlController {
         );
     }
 
+
+    @AllArgsConstructor
+    @Getter
+    public static class SurlGetItemsRespBody {
+        private List<SurlDto> items;
+    }
+    @GetMapping("")
+    public RsData<SurlGetItemsRespBody> getItems() {
+        Member member = rq.getMember();
+        List<Surl> surls = surlService.findByAuthorOrderByIdDesc(member);
+        // Page
+        // QueryDSL
+        return RsData.of(
+                new SurlGetItemsRespBody(
+                        surls.stream()
+                                .map(SurlDto::new)
+                                .toList()
+                )
+        );
+    }
+
     @DeleteMapping("/{id}")
     @Transactional
     public RsData<Empty> delete(
@@ -90,5 +113,4 @@ public class ApiV1SurlController {
 
         return RsData.OK;
     }
-
 }

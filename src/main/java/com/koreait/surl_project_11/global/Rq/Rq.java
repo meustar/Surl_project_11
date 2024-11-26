51e92386791b8cec4576674f78628a4f978964f4
meustar <meustar829@gmail.com>
2024-11-26 13:22:36 +0900

    Authorization Header

--- src/main/java/com/koreait/surl_project_11/global/Rq/Rq.java
+++ src/main/java/com/koreait/surl_project_11/global/Rq/Rq.java
@@ -28,8 +28,27 @@ public class Rq {
     public Member getMember() {
         if(member != null) return member;   // 캐시 데이터 방식. == 메모리 캐싱
 
+        // 1. Url parameter에서 받아오는 방법.
         String actorUsername = req.getParameter("actorUsername"); // HttpServletRequest req => @RequestScope  .. Rq는 @RequestScope의 Bean이다.
         String actorPassword = req.getParameter("actorPassword");
+
+        if (actorUsername == null || actorPassword == null) {
+            String authorization = req.getHeader("Authorization");
+            if(authorization != null) {
+                authorization = authorization.substring("bearer ".length());
+                String[] authorizationBits = authorization.split(" ", 2);
+                actorUsername = authorizationBits[0];
+                actorPassword = authorizationBits.length == 2 ? authorizationBits[1] : null;
+            }
+        }
+
+        // 3. 파라미터와 해더를 같이쓰는 방법.
+//        if(actorUsername == null) actorUsername = req.getHeader("actorUsername");
+//        if(actorPassword == null) actorPassword = req.getHeader("actorPassword");
+
+        // 1. header에서 받아오는 방법.
+//        actorUsername = req.getHeader("actorUsername");
+//        actorPassword = req.getHeader("actorPassword");
 
         if(Ut.str.isBlank(actorUsername)) throw new GlobalException("401-1", "인증정보(아이디) 입력해주세요.");
         if(Ut.str.isBlank(actorPassword)) throw new GlobalException("401-2", "인증정보(비밀번호) 입력해주세요.");

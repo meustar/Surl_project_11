1a8627e63081852d81f1c2e9a84a93cd2516cc25
meustar <meustar829@gmail.com>
2024-11-27 14:33:46 +0900

    인증정보 더블체크 제거

--- src/main/java/com/koreait/surl_project_11/global/Rq/Rq.java
+++ src/main/java/com/koreait/surl_project_11/global/Rq/Rq.java
@@ -2,12 +2,11 @@ package com.koreait.surl_project_11.global.Rq;
 
 import com.koreait.surl_project_11.domain.member.member.entity.Member;
 import com.koreait.surl_project_11.domain.member.member.service.MemberService;
-import com.koreait.surl_project_11.global.exceptions.GlobalException;
-import com.koreait.surl_project_11.standard.util.Ut;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
@@ -25,35 +24,41 @@ public class Rq {
     private Member member;
 
     public Member getMember() {
-        if(member != null) return member;   // 캐시 데이터 방식. == 메모리 캐싱
+        if (member != null) return member;   // 캐시 데이터 방식. == 메모리 캐싱
 
-        // cookie
-        String actorUsername = getCookieValue("actorUsername", null);
-        String actorPassword = getCookieValue("actorPassword", null);
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
 
-        // 4.
-        if (actorUsername == null || actorPassword == null) {
-            String authorization = req.getHeader("Authorization");
-            if(authorization != null) {
-                authorization = authorization.substring("bearer ".length());
-                String[] authorizationBits = authorization.split(" ", 2);
-                actorUsername = authorizationBits[0];
-                actorPassword = authorizationBits.length == 2 ? authorizationBits[1] : null;
-            }
-        }
+        member = memberService.findByUsername(name).get();
 
-        if(Ut.str.isBlank(actorUsername)) throw new GlobalException("401-1", "인증정보(아이디) 입력해주세요.");
-        if(Ut.str.isBlank(actorPassword)) throw new GlobalException("401-2", "인증정보(비밀번호) 입력해주세요.");
+        return member;
 
-        Member loginedMember = memberService.findByUsername(actorUsername).orElseThrow(() -> new GlobalException("403-3", "해당 회원은 없습니다."));
-
-        // 비밀번호 인증에 인코더 적용
-        if (!memberService.matchPassword(actorPassword, loginedMember.getPassword()))
-            throw new GlobalException("403-4", "비밀번호 틀림");
-
-        member = loginedMember;
-
-        return loginedMember;
+//        // cookie
+//        String actorUsername = getCookieValue("actorUsername", null);
+//        String actorPassword = getCookieValue("actorPassword", null);
+//
+//        // 4.
+//        if (actorUsername == null || actorPassword == null) {
+//            String authorization = req.getHeader("Authorization");
+//            if(authorization != null) {
+//                authorization = authorization.substring("bearer ".length());
+//                String[] authorizationBits = authorization.split(" ", 2);
+//                actorUsername = authorizationBits[0];
+//                actorPassword = authorizationBits.length == 2 ? authorizationBits[1] : null;
+//            }
+//        }
+//
+//        if(Ut.str.isBlank(actorUsername)) throw new GlobalException("401-1", "인증정보(아이디) 입력해주세요.");
+//        if(Ut.str.isBlank(actorPassword)) throw new GlobalException("401-2", "인증정보(비밀번호) 입력해주세요.");
+//
+//        Member loginedMember = memberService.findByUsername(actorUsername).orElseThrow(() -> new GlobalException("403-3", "해당 회원은 없습니다."));
+//
+//        // 비밀번호 인증에 인코더 적용
+//        if (!memberService.matchPassword(actorPassword, loginedMember.getPassword()))
+//            throw new GlobalException("403-4", "비밀번호 틀림");
+//
+//        member = loginedMember;
+//
+//        return loginedMember;
     }
 
     public String getCurrentUrlPath() {

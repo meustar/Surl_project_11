091769f7e8896248258714c63b486c053f8b5a6d
meustar <meustar829@gmail.com>
2024-12-05 17:25:06 +0900

    일반 Controller 문서 꾸미기

--- src/main/java/com/koreait/surl_project_11/domain/home/home/controller/HomeController.java
+++ src/main/java/com/koreait/surl_project_11/domain/home/home/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.koreait.surl_project_11.domain.home.home.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,22 +10,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @Transactional(readOnly = true)
+@Tag(name = "HomeController", description = "홈 컨트롤러")
 public class HomeController {
     @Value("${custom.site.name}")
     private String customSiteName;
 
-    @Value("${custom.secret.key}")
-    private String secretKey;
-
     @GetMapping("/")
     @ResponseBody
+    @Operation(summary = "API 메인화면")
     public String ShowMain(){
         return "Meustar~  " + customSiteName + "~!! Wow";
-    }
-
-    @GetMapping("/secretKey")
-    @ResponseBody
-    public String ShowSecretKey() {
-        return "secretKey : " + secretKey;
     }
 }

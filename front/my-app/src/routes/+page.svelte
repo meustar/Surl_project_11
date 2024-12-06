0a8ec3c5c828f1b7b93d5b446d0e77ef74587df4
meustar <meustar829@gmail.com>
2024-12-06 15:21:02 +0900

    레이아웃

--- front/my-app/src/routes/+page.svelte
+++ front/my-app/src/routes/+page.svelte
@@ -1,7 +1,2 @@
-<ul>
-  <li><a href="/">메인</a></li>
-  <li><a href="/about">설명</a></li>
-  <li><a href="/home/links">링크</a></li>
-</ul>
 <h1>Main page</h1>
 
--- front/my-app/src/routes/home/links/+page.svelte
+++ front/my-app/src/routes/home/links/+page.svelte
@@ -1,6 +1,1 @@
-<ul>
-  <li><a href="/">메인</a></li>
-  <li><a href="/about">설명</a></li>
-  <li><a href="/home/links">링크</a></li>
-</ul>
 <h1>home/links</h1>
